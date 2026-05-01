c77df6c5e7a7672c003465a736632d511f16909c
admin <admin@DESKTOP-K4H3P0H>
2026-05-01 12:53:33 +0530

    Add project files.

---
+++ productcontroller/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace productcontroller.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET:Product
+        public ActionResult Index()
+        {
+            int productid = 9000;
+            String Name = "apple";
+            string Quantity = "1kg";
+            Decimal Price = 199.99m;
+            bool status = true;
+
+            ViewData["productid"]=productid;
+            ViewData["name"]=Name;
+            ViewData["quantity"]=Quantity;
+            ViewData["price"]=Price;
+            ViewData["status"]=status;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ productcontroller/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="productcontroller.MvcApplication" Language="C#" %>
